5276da5ba81600f82eabbfb88dd09d671073cdfb
rahulshahare <rahulshahare@gmail.com>
2020-05-17 23:47:25 +0530

    state for app component is initialize

--- src/counter/counter.js
+++ src/counter/counter.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './counter.css';
 
 export default class Counter extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            count:0
+        }
+    }
     render(){
         return(
             <h1>Counter</h1>
