e05502fe2f3fe7b808e8622d4bcf9bb7f14ce11b
rahulshahare <rahulshahare@gmail.com>
2020-05-17 23:45:10 +0530

    counter app is initialize

---
+++ src/counter/counter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './counter.css';
+
+export default class Counter extends React.Component{
+    render(){
+        return(
+            <h1>Counter</h1>
+        )
+    }
+}
