cab05f08649e4f93aca67f0d3bf4e77dba0ffacc
rahulshahare <rahulshahare@gmail.com>
2020-05-27 16:20:12 +0530

    goinfNext

--- src/App.js
+++ src/App.js
@@ -2,28 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import  'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Counter from './counter/counter'
 
 function App() {
   return (
     <div className="App text-success">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          React Counter App
-        </h1>
-        <Counter/>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
     </div>
   );
 }
