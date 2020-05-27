8d56e46fdf1a0c89d2b79eb46b492948719205c1
rahulshahare <rahulshahare@gmail.com>
2020-05-28 00:16:10 +0530

    aboutPage

---
+++ src/component/about/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class AboutPage extends React.Component{
+    render(){
+        return(
+            <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Fluid jumbotron</h1>
+                <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            </div>
+            </div>
+        )
+    }
+}
