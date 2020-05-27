44ac0747e31260ab0838c85b32133e89b6696e28
rahulshahare <rahulshahare@gmail.com>
2020-05-28 00:15:56 +0530

    text center

--- src/component/home/home.js
+++ src/component/home/home.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class HomePage extends React.Component{
     render(){
         return(
-            <div className="jumbotron">
+            <div className="jumbotron text-center">
                 <h1 className="display-4">Hello, world!</h1>
                 <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
                 <hr className="my-4"/>
