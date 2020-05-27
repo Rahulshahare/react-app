b948f2a8cc25c3c250292bedd62e76c114c37365
rahulshahare <rahulshahare@gmail.com>
2020-05-28 01:45:12 +0530

    404

--- src/component/router/router.js
+++ src/component/router/router.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
 import HomePage from '../home/home';
 import AboutPage from '../about/about';
-
+import Page404 from '../404';
 export default class Routers extends React.Component{
     render(){
         return(
@@ -10,6 +10,7 @@ export default class Routers extends React.Component{
                 <Switch>
                     <Route exact={true} path="/" component={HomePage}></Route>
                     <Route path="/about" component={AboutPage}></Route>
+                    <Route path="*" component={Page404}></Route>
                 </Switch>
             </Router>
         )
