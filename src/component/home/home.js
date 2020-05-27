5a384723ddf746eb780f35e2235f2669afc698f5
rahulshahare <rahulshahare@gmail.com>
2020-05-28 02:46:13 +0530

    title and subtitle

--- src/component/home/home.js
+++ src/component/home/home.js
@@ -5,8 +5,8 @@ export default class HomePage extends React.Component{
     render(){
         return(
             <div className="jumbotron text-center">
-                <h1 className="display-4">Hello, world!</h1>
-                <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <h1 className="display-4">Welcome To LocalOlx</h1>
+                <p className="lead">Showcase your product from Galli to Delhi</p>
                 <hr className="my-4"/>
                 <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
                 <Link to="/about" className="btn btn-primary btn-lg">Home</Link>
