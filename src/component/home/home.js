add1809c3268954d83920b60434dcc489a524d32
rahulshahare <rahulshahare@gmail.com>
2020-05-28 02:32:13 +0530

    Link item

--- src/component/home/home.js
+++ src/component/home/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class HomePage extends React.Component{
     render(){
@@ -8,7 +9,7 @@ export default class HomePage extends React.Component{
                 <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
                 <hr className="my-4"/>
                 <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
-                <a className="btn btn-primary btn-lg" role="button" href="/about">Learn more</a>
+                <Link to="/about" className="btn btn-primary btn-lg">Home</Link>
             </div>
         );
     }
