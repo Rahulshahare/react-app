a7864028b42f761c00cf5f407f99481b3065e665
rahulshahare <rahulshahare@gmail.com>
2020-05-28 00:16:30 +0530

    home nd about page are on app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import  'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import HomePage from './component/home/home';
+import AbouPage from './component/about/about';
 
 function App() {
   return (
-    <div className="App text-success">
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12">
+          <HomePage/>
+          <AbouPage/>
+        </div>
+      </div>
     </div>
   );
 }
