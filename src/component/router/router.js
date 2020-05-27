50625342d9255efec19d768bf013bc19c2fb8f6e
rahulshahare <rahulshahare@gmail.com>
2020-05-28 02:33:19 +0530

    Router  is arrange

--- src/component/router/router.js
+++ src/component/router/router.js
@@ -3,15 +3,23 @@ import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
 import HomePage from '../home/home';
 import AboutPage from '../about/about';
 import Page404 from '../404';
+import Navbar from '../common/navbar';
 export default class Routers extends React.Component{
     render(){
         return(
             <Router>
-                <Switch>
-                    <Route exact={true} path="/" component={HomePage}></Route>
-                    <Route path="/about" component={AboutPage}></Route>
-                    <Route path="*" component={Page404}></Route>
-                </Switch>
+                <div className="container-fluid">
+                    <Navbar/>
+                    <div className="row">
+                        <div className="col-12">
+                                <Switch>
+                                    <Route exact={true} path="/" component={HomePage}></Route>
+                                    <Route path="/about" component={AboutPage}></Route>
+                                    <Route path="*" component={Page404}></Route>
+                                </Switch>
+                        </div>
+                    </div>
+                </div>
             </Router>
         )
     }
