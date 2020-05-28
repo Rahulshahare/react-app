f0f6b532f6aa03ac039487423f9ee360643e838c
rahulshahare <rahulshahare@gmail.com>
2020-05-28 18:19:36 +0530

    loading...

--- src/component/courses/courses.js
+++ src/component/courses/courses.js
@@ -4,15 +4,15 @@ const CreateCoursePage = React.lazy(()=>import('./createCourse'));
 function CoursePage (){
     return(
         <div>
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-4">Courses</h1>
-                <p class="lead">We can Create list of Courses here</p>
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1 class="display-4">Courses</h1>
+                    <p class="lead">We can Create list of Courses here</p>
+                </div>
             </div>
-        </div>
-        <Suspense fallback={<div>Loading...</div>}>
-            <CreateCoursePage/>
-        </Suspense>
+            <Suspense fallback={<div>Loading...</div>}>
+                <CreateCoursePage/>
+            </Suspense>
         </div>
     )
 }
