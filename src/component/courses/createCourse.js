12df78b3b1295048c3e97ea202f8877518548401
rahulshahare <rahulshahare@gmail.com>
2020-05-29 00:17:00 +0530

    for

--- src/component/courses/createCourse.js
+++ src/component/courses/createCourse.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
 class CreateCoursePage extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            courseName:''
+        }
+    }
     render(){
         return(
-            <h1>Add New Course Here</h1>
+            <div>
+                <h1>Add New Course Here</h1>
+                <form >
+                    <div class="form-group">
+                        <label for="course">Course</label>
+                        <input type="text" class="form-control" id="course"/>
+                    </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
         )
     }
 }
