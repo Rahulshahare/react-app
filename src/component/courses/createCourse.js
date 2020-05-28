8f2906b59cf0e00186f4b071f5299fa68f8d4465
Rahul Shahare <rahulshahare@gmail.com>
2020-05-29 00:55:52 +0530

    onSubmit course name is added to array.

--- src/component/courses/createCourse.js
+++ src/component/courses/createCourse.js
@@ -4,17 +4,32 @@ class CreateCoursePage extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            courseName:''
+            courseName:'',
+            courseList:[]
         }
     }
+    handleChange = (event) =>{
+        this.setState({
+            courseName:event.target.value
+        })
+    }
+    handleSubmit = () =>{
+        event.preventDefault()
+        const a = this.state.courseList;
+        a = a.push(this.state.courseName);
+
+        this.setState({
+            courseList:a
+        })
+    }3
     render(){
         return(
             <div>
                 <h1>Add New Course Here</h1>
-                <form >
+                <form onSubmit={this.handleSubmit}>
                     <div class="form-group">
                         <label for="course">Course</label>
-                        <input type="text" class="form-control" id="course"/>
+                        <input type="text" onChange={this.handleChange} class="form-control" id="course"/>
                     </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
