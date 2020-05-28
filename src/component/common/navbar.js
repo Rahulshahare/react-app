3a04622784d447066a5f6228cda4c7923e3a2a98
Rahul Shahare <rahulshahare@gmail.com>
2020-05-29 01:37:43 +0530

    class is not allowed keyword in react

--- src/component/common/navbar.js
+++ src/component/common/navbar.js
@@ -3,7 +3,7 @@ import { NavLink , } from 'react-router-dom';
 
 function Navbar(){
     return(
-        <nav class="nav">
+        <nav className="nav">
             <NavLink to="/" className="nav-link" activeClassName="active">Home</NavLink>
             <NavLink to="/about" className="nav-link" activeClassName="active">About</NavLink>
             <NavLink to="/courses" className="nav-link" activeClassName="active">Courses</NavLink>
--- src/component/courses/courses.js
+++ src/component/courses/courses.js
@@ -4,10 +4,10 @@ const CreateCoursePage = React.lazy(()=>import('./createCourse'));
 function CoursePage (){
     return(
         <div>
-            <div class="jumbotron jumbotron-fluid">
-                <div class="container">
-                    <h1 class="display-4">Courses</h1>
-                    <p class="lead">We can Create list of Courses here</p>
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4">Courses</h1>
+                    <p className="lead">We can Create list of Courses here</p>
                 </div>
             </div>
             <Suspense fallback={<div>Loading...</div>}>
