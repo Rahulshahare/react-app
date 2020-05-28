31a6f2314805747a6ad2a31599f24b1ff205294f
rahulshahare <rahulshahare@gmail.com>
2020-05-28 23:32:21 +0530

    its demo purpose

--- src/component/router/router.js
+++ src/component/router/router.js
@@ -1,17 +1,15 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
 import HomePage from '../home/home';
 import AboutPage from '../about/about';
-//import CoursePage from '../courses/courses';
+import CoursePage from '../courses/courses';
 import Page404 from '../notFound/404';
 import Navbar from '../common/navbar';
-const CoursePage = React.lazy(() => import('../courses/courses'));
 
 export default class Routers extends React.Component{
     render(){
         return(
             <Router>
-                <Suspense fallback={<div>Loading...</div>}>
                 <div className="container-fluid">
                     <Navbar/>
                     <div className="row">
@@ -25,7 +23,6 @@ export default class Routers extends React.Component{
                         </div>
                     </div>
                 </div>
-                </Suspense>
             </Router>
         )
     }
