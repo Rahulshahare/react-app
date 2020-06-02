66ec42b1f90a52a20398f219ba43d736ecaa264f
rahulshahare <rahulshahare@gmail.com>
2020-06-03 04:51:40 +0530

    currying takes N argument and return single result

--- src/currying.js
+++ src/currying.js
@@ -1,3 +1,12 @@
 /**
  * Lets learn a bit about Currying
  */
+
+ function add(a, b){
+     return a + b;
+ }
+
+ /**
+  * Currying is technique which take N arguments 
+  * and Converted i.e. return the result in sungle argumwnt
+  */
