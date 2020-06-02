2cea5d3a4acff183090fed66d1c61d6686b3ae3f
rahulshahare <rahulshahare@gmail.com>
2020-06-02 14:37:33 +0530

    funtion are first class ciizens

--- src/index.js
+++ src/index.js
@@ -8,4 +8,14 @@ function greet(fnMessage) {
     }
 }
 
-let fn = sayHello(); // here Assign a function to Variable
+let fn = greet(); // here Assign a function to Variable
+
+let message = fn(); //Assignning Returning Annonomous function to Variable
+console.log(message);
+
+/**
+ * Here returning a annonomous funtion from a function
+ * and assigning to variable and then calling it.
+ * using function like variable here
+ * Function are first class citizens
+ */
