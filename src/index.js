6309f30fcc9e87141a956c788de767e714a0838f
rahulshahare <rahulshahare@gmail.com>
2020-06-02 15:11:31 +0530

    creating functions for tasks

---
+++ src/index.js
@@ -0,0 +1,23 @@
+let input = "    Javascript   ";
+
+/**
+ * Mission is to trim the input
+ * and Wrap it in Div
+ */
+
+ let output = "<div>"+ input.trim() +"</di>";
+
+ /**
+  * lets Solve this with functional Programming
+  */
+
+  function trimStr(str){
+      return str.trim()
+  }
+  const trimStr = str => str.trim(); //MoreAdance
+
+  function wrapInDiv(str){
+      return "<div>"+ str +"</div>"
+  }
+
+  const wrapInDiv = str =>"<div>"+ str +"</div>"; //MoreAdane
