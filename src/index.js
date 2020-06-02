7fcecd9037797428db16fcddb6707e500e182d4a
rahulshahare <rahulshahare@gmail.com>
2020-06-03 04:42:10 +0530

    wrap is a dynamic function

--- src/index.js
+++ src/index.js
@@ -24,31 +24,13 @@ let input = "    Javascript   ";
 
   //these are reusable function
 
-  const result = wrapInDiv(toLowerCase(trimStr(input)));
-  //its Functional Composition, toLowerCase takes the result of trimStr and convert it to lowercase
-  //Anather level of Function Composition
-  /** Here we are reading above is like right to left,
-   * first trimStr then convert the result to lowercase
-   * and then wrap it into div.
-   */
 
 
-  const transform =  compose(wrapInDiv, toLowerCase, trimStr);
-  console.log(transform(input));
-  /**
-   * lodash compose also read from right to left
-   * first trimStr then it gonna pass it from toLowerCase then 
-   * wrap it in div.
-   * lodash compose will be usefull if we have more funtions to procced
-   */
-
-  const transformx = pipe(trimStr, toLowerCase, wrapInDiv);
+  const transformx = pipe(trimStr, toLowerCase, wrap);
   console.log(transformx(input));
 
    /**
-    * Now with pipe lodash function its easy for us to read functions.
-    * from start to end
-    * let to right
+    * 
     */
 
  
