a0eec5139eb7765e07fdbe306c792ed581817690
rahulshahare <rahulshahare@gmail.com>
2020-06-02 14:20:39 +0530

    passing function as parameter and callin it

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ function sayHello() {
     return "Hello WOrld";
 }
 
+function greet(fnMessage) {
+    console.log(fnMessage());
+}
+//greet function getting function as paremater and calling it here
