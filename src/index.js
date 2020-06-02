401270fa879738f7cb2d4261da60c41ff292253d
rahulshahare <rahulshahare@gmail.com>
2020-06-02 14:52:52 +0530

    we take each number from array and multiplying it with 2

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ let fn = sayHello(); // here Assign a function to Variable
 let message = fn(); //Assignning Returning Annonomous function to Variable
 console.log(message);
 
-/**
- * Here sayHello and greet are Higher Order Functions,
- * which can take funtion as a Argument or return a function
- */
+let numbers = [1,2,3];
+numbers.map(number => number * 2) 
+//Here map is Higher order funtion, so we take each number and multiplied by 2
