6c276997865579b5abe18f62cb11862afabc150d
rahulshahare <rahulshahare@gmail.com>
2020-06-03 06:33:37 +0530

    target, Sourceand optionally updated properties

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const person = { name: "Wizkumar" } //person is a object with name property
 
-const tar = Object.assign({} ,person); // this gonna copy all properties of person object to empty object
+const tar = Object.assign({} ,person); 
+// Object.assign takes 3 arguments, target object, Source object and optionally we can pass updated properites to this object
 
 console.log(tar)
