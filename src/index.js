c828c1561366c05c9d6383ae3b4e08062aa56368
rahulshahare <rahulshahare@gmail.com>
2020-06-03 09:06:12 +0530

    handling object with immer

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+let book = { title: "Shivaji Maharaj"};
+
+function publish(book){
+    book.isPublished =  true; //this way add new property called isPublished and set value to true
+}
+
+publish(book);
+
+console.log(book);
