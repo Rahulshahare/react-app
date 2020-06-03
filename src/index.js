c8bc45216b9f9eb08a2e26cbda5f34799d358ebf
rahulshahare <rahulshahare@gmail.com>
2020-06-03 10:59:35 +0530

    creating multiple dispatch is as easy as globe

--- src/index.js
+++ src/index.js
@@ -1,30 +1,15 @@
 import store from './store';
-import * as actions from './actionTypes';
+import { courseAdded } from './actions';
 
 const unsubcribe = store.subscribe( ()=>{
     console.log("Store Change", store.getState());
 });
 
-store.dispatch({
-    type: actions.COURSE_ADDED,
-    payload:{
-        courseName: "first Course Name"
-    }
-});
-store.dispatch({
-    type: actions.COURSE_ADDED,
-    payload:{
-        courseName: "first Course Name"
-    }
-});
+store.dispatch(courseAdded("First Course Name"));
+store.dispatch(courseAdded("Second Course Name"));
+
 
 unsubcribe(); // after this we are not going to get notified if the store changes
 
-store.dispatch({
-    type: actions.COURSE_REMOVED,
-    payload:{
-        id: 1
-    }
-})
 
 console.log(store.getState());
