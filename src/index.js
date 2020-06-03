fe3c82039588bf7ca49b4d0158258c766e16a564
rahulshahare <rahulshahare@gmail.com>
2020-06-03 08:39:00 +0530

    this way able to get title of object

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
-let book = Map({ title: "Shivaji Maharaj" });
+let book = Map({ title: "Shivaji Maharaj"});
 
 // function publish(book){
 //     book.isPublished = true; //this add new property called isPublished and set value to true
