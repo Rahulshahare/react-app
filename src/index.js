701700a46121228058775cf02e485b5080e588cd
rahulshahare <rahulshahare@gmail.com>
2020-06-03 06:43:51 +0530

    playing more with properties of object

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const person = { name: "Wizkumar" } //person is a object with name property
 
-const tar = Object.assign({} ,person, { name: "Javascript", age: 25, pets : { name:"wiz" , color:"Black" } }); 
+const tar = Object.assign({} ,person, { name: "Javascript", age: 25, pets : { name:"wiz" , color:"Black" }, list:["A", "B", "C"] }); 
 // Object.assign takes 3 arguments, target object, Source object and optionally we can pass updated properites to this object
 
-console.log(tar)
+
+tar.list.map(a =>{
+    console.log(a)
+})
+
+// mapping array from object
