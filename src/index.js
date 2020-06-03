693960605940821c10a99f17b56f1b081d37a851
rahulshahare <rahulshahare@gmail.com>
2020-06-03 06:35:13 +0530

    Also able to add additional properties

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const person = { name: "Wizkumar" } //person is a object with name property
 
-const tar = Object.assign({} ,person, { name: "Javascript" }); 
+const tar = Object.assign({} ,person, { name: "Javascript", age: 25 }); 
 // Object.assign takes 3 arguments, target object, Source object and optionally we can pass updated properites to this object
 
 console.log(tar)
