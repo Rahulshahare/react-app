84cc7bc930e03a851f29a0993fca86154b40cc0d
rahulshahare <rahulshahare@gmail.com>
2020-06-03 07:03:06 +0530

    new property address is added to person object

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-const person = { name: "Wizkumar" } //person is a object with name property
+const person = { 
+    name: "Wizkumar", 
+    address:{
+        country: "India",
+        city: "Mumbai"
+    }
+} //person is a object with name property
 
 const updated = {...person, name:"javascript"}
+
+
 
 console.log( updated );
