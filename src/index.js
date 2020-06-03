594d21add647db2cfdafd626567fce4d10cf33df
rahulshahare <rahulshahare@gmail.com>
2020-06-03 08:10:38 +0530

    functional programming with objects

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let book = { tile: "Shivaji Maharaj" };
+
+function publish(book){
+    book.isPublished = true; //this add new property called isPublished and set value to true
+}
+
+publish(book);
+
+console.log(book);
