86ed70fad258896fc9fc65e9d311bc6c40ea5c1c
rahulshahare <rahulshahare@gmail.com>
2020-06-03 09:28:53 +0530

    lets get on redux

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * 4 steps while follow building redux app
+ * 
+ * 1. Design the store - decide what to keep in store
+ * 2. Design the actions - actions that user can perform in this application
+ * 3. Create a reducer - this reducer take and action and return the new state
+ * 4. Set up the store - set store based on reducer
+ */
