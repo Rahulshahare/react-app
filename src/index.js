38855419d415c87a94ef7928517792312f21a428
rahulshahare <rahulshahare@gmail.com>
2020-06-03 06:55:42 +0530

    using Object.assign, we can copy source object to new target objet and add new properties

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 const person = { name: "Wizkumar" } //person is a object with name property
 
-const tar = Object.assign({} ,person, { name: "Javascript", age: 25, pets : { name:"wiz" , color:"Black" }, list:["A", "B", "C"] }); 
+const updated = Object.assign({} ,person, { name: "Javascript", age: 25, pets : { name:"wiz" , color:"Black" }, list:["A", "B", "C"] }); 
 // Object.assign takes 3 arguments, target object, Source object and optionally we can pass updated properites to this object
 
 
-tar.list.map(a =>{
-    console.log( toLowerCase(a) )
-})
-
-// mapping array from object
-
-function toLowerCase(str){
-    return str.toLowerCase()
-}
+console.log( updated );
