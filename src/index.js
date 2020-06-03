2c5dc89881afca08f152f667869713879bc3d2c9
rahulshahare <rahulshahare@gmail.com>
2020-06-03 08:42:40 +0530

    adding mew property using immutable

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import { Map } from 'immutable';
 
 let book = Map({ title: "Shivaji Maharaj"});
 
-// function publish(book){
-//     book.isPublished = true; //this add new property called isPublished and set value to true
-// }
+function publish(book){
+    return book.set("isPublished", true); //this way add new property called isPublished and set value to true
+}
 
-// publish(book);
+book = publish(book);
 
 console.log(book.toJS());
