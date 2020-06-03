47bfeee9bd0ed2acb288f56b159e39c13d660f4f
rahulshahare <rahulshahare@gmail.com>
2020-06-03 07:07:04 +0530

    both objets reference to same dats why it changes both the objects

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ const person = {
 
 const updated = {...person, name:"javascript"}
 
+updated.address.city = "Taiwan";
 
+console.log( person );
 
-console.log( updated );
+/**
+ * Here we have updated the property of new object but as it our both
+ * objects person & updated reference to same so the person object is also change
+ * 
+ * as we want to update the new properties to new object
+ */
