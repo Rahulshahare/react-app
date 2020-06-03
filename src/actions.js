223f68350375fbd5a3df512c7c6198f155ffabd7
rahulshahare <rahulshahare@gmail.com>
2020-06-03 10:59:04 +0530

    courseAdded function get courseName as argument as return with type and payload model

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,16 @@
+import * as actions from './actionTypes';
 /**
  * Lets create action creators 
  * if wa wann dispatch same action multiple time,
  * writing the same code is not a solution 
  * so lets create here
  */
+
+export function courseAdded(courseName){
+    return{
+        type: actions.COURSE_ADDED,
+        payload:{
+            courseName: courseName
+        }
+    }
+ }
