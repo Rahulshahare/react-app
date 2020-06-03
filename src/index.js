9a6caeee1b89f1d1e2f4cd84cc286266897aadb3
rahulshahare <rahulshahare@gmail.com>
2020-06-03 06:34:40 +0530

    Here we are able to update properties of object

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const person = { name: "Wizkumar" } //person is a object with name property
 
-const tar = Object.assign({} ,person); 
+const tar = Object.assign({} ,person, { name: "Javascript" }); 
 // Object.assign takes 3 arguments, target object, Source object and optionally we can pass updated properites to this object
 
 console.log(tar)
