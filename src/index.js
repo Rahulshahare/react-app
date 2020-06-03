2d84c958c30640d708f660d44d9a9fcdbe744620
rahulshahare <rahulshahare@gmail.com>
2020-06-03 06:56:32 +0530

    Now lets use spread operator to do our job

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const person = { name: "Wizkumar" } //person is a object with name property
 
-const updated = Object.assign({} ,person, { name: "Javascript", age: 25, pets : { name:"wiz" , color:"Black" }, list:["A", "B", "C"] }); 
-// Object.assign takes 3 arguments, target object, Source object and optionally we can pass updated properites to this object
-
+const updated = 
 
 console.log( updated );
