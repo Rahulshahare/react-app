9017f65ab8478186fde1246b9ccc4a3ba3677299
rahulshahare <rahulshahare@gmail.com>
2020-06-04 23:15:08 +0530

    Associate operation for course list

--- src/component/redux/reducer.js
+++ src/component/redux/reducer.js
@@ -1,3 +1,18 @@
 function reducer(state = [], action){
 
+    switch(action.type){
+        case 'addingCourse':
+            return[
+                ...state,
+                {
+                    coursename: action.payload.coursename
+                }
+            ]
+
+        case 'resetCourse':
+            return state = [];
+
+        default:
+            return state;
+    }
 }
