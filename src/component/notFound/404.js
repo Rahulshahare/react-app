67d39f6a9cadbc0f739fa95fb5617d50cbc30c4c
rahulshahare <rahulshahare@gmail.com>
2020-06-04 23:37:37 +0530

    className?

--- src/component/notFound/404.js
+++ src/component/notFound/404.js
@@ -3,10 +3,10 @@ import {Link } from 'react-router-dom';
 
 function Page404(){
     return(
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-4">its 404</h1>
-                <p class="lead">The page you are looking for is not found.</p>
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">its 404</h1>
+                <p className="lead">The page you are looking for is not found.</p>
                 <Link to="/" className="btn btn-primary btn-lg">Home</Link>
             </div>
         </div>
