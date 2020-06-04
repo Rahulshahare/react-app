16096e9b8a403208e38718a6547b3da3e94d3f40
rahulshahare <rahulshahare@gmail.com>
2020-06-04 22:50:25 +0530

    rearrange

--- src/redux/counterReducer.js
+++ src/redux/counterReducer.js
@@ -1,14 +1,17 @@
 import * as actionType from './actionTypes';
 export default function reducer(state = 0, action){
     switch(action.type){
+        
         case actionType.INCREASE:
             return state + 1;
+
         case actionType.DECREASE:
             if(state >= 1){
                 return state - 1;
             }else{
                 return state;
             }
+
         default: 
             return state;
     }
