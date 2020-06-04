597a5656200af8b1d7b4496b2fb0f9bbccc6c871
rahulshahare <rahulshahare@gmail.com>
2020-06-05 01:38:13 +0530

    data can flow through the app

--- src/component/home/home.js
+++ src/component/home/home.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CreateCoursePage from '../courses/createCourse';
 
 export default class HomePage extends React.Component{
     render(){
         return(
+            <div>
             <div className="jumbotron text-center">
                 <h1 className="display-4">Welcome To LocalOlx</h1>
                 <p className="lead">Showcase your product from Galli to Delhi</p>
                 <hr className="my-4"/>
-                <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
+                <p>Here Testing Redux. data can flow through app.below component have a data</p>
                 <Link to="/about" className="btn btn-primary btn-lg">Home</Link>
+            </div>
+            <CreateCoursePage/>
             </div>
         );
     }
