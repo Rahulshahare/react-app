4e0d8666aab6005c0490ab7b5e341a6f82b63bd2
rahulshahare <rahulshahare@gmail.com>
2020-06-05 01:05:37 +0530

    deleting item of array but its value, here is reducer

--- src/component/redux/reducer.js
+++ src/component/redux/reducer.js
@@ -4,7 +4,20 @@ export default function reducer(state = [], action){
 
     switch(action.type){
         case actionType.ADD_NEW_COURSE:
-            return state = state.concat(action.payload.coursename)
+            // return[
+            //     ...state,
+            //     action.payload.coursename
+            // ]
+            return state.concat(action.payload.coursename)
+
+        case actionType.DELETE_COURSE:
+            
+            const index = state.indexOf(action.payload.coursename);
+            if (index > -1) {
+              state.splice(index, 1);
+            }
+        
+            break;
 
         case actionType.RESET_COURSE :
             return state = [];
