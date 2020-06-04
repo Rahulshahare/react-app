a32d2f921ec21a79f6fb140245ab6fac80e7041e
rahulshahare <rahulshahare@gmail.com>
2020-06-05 03:11:11 +0530

    adding user data

--- src/component/redux/userReducer.js
+++ src/component/redux/userReducer.js
@@ -4,10 +4,7 @@ export default function UserReducer(state = [], action){
 
     switch(action.type){
         case actionType.ADD_NEW_USER:
-            return[
-                ...state,
-                action.payload.users
-            ]
+            return state = action.payload.user
 
         default: return state
     }
