0916e161260026cb0a56c95b3e70eb5365b637f4
Rahulshahare <rahulshahare@gmail.com>
2020-06-13 20:23:49 +0530

    Provider from react-redux and store are importted

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import  'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Routers from './component/router/router';
+import { Provider } from 'react-redux';
+import store from './component/redux/store';
+
 
 function App() {
   return (
