59a5e8ba1b8660fbac091faafa06438e3ba964ac
Rahulshahare <rahulshahare@gmail.com>
2020-06-13 19:16:20 +0530

    all reducer combine together to form a reducer for app

--- src/component/redux/reducer.js
+++ src/component/redux/reducer.js
@@ -1,24 +1,11 @@
-import * as actionType from './actionTypes';
+import { combineReducers } from 'redux';
+import courses from './courseReduer';
+import users from './userReducer';
 
-export default function reducer(state = [], action){
+const reducer = combineReducers({
+    courses,
+    users
+})
 
-    switch(action.type){
-        case actionType.ADD_NEW_COURSE:
-            // return[
-            //     ...state,
-            //     action.payload.coursename
-            // ]
-            return state.concat(action.payload.coursename)
 
-        case actionType.DELETE_COURSE:
-            // console.log( action.payload.coursename);
-            return state.filter(n => n !== action.payload.coursename)
-        
-
-        case actionType.RESET_COURSE :
-            return state = [];
-
-        default:
-            return state;
-    }
-}
+export default reducer;
