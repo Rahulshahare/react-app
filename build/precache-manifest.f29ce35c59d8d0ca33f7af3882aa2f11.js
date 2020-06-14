02ff36c2db1bcd5c0513a58f9529df2984156420
Rahulshahare <rahulshahare@gmail.com>
2020-06-14 13:51:30 +0530

    The first Build

---
+++ build/precache-manifest.f29ce35c59d8d0ca33f7af3882aa2f11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc3fbbefe4342e92f26bf9baa0c54da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "010cf2c70fc73e43e91b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5c697b8d3bf4a2e437b9",
+    "url": "/static/css/main.60435983.chunk.css"
+  },
+  {
+    "revision": "010cf2c70fc73e43e91b",
+    "url": "/static/js/2.96ab22ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.96ab22ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c697b8d3bf4a2e437b9",
+    "url": "/static/js/main.e8f5d170.chunk.js"
+  },
+  {
+    "revision": "e09b3a133424f3eaaad2",
+    "url": "/static/js/runtime-main.d7a593f2.js"
+  }
+]);
